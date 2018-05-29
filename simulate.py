bc9466a7d41d6a390a07a33799818190af9b41c3
Artem Stepanov <zergatull@gmail.com>
2018-05-29 12:11:21 +0300

    some optimizations

--- simulate.py
+++ simulate.py
@@ -3,18 +3,21 @@ import time
 import sys
 
 
-def myChoice(selectList, x, node):
+def my_сhoice_sample(selectList, x, *argv):
+    node = argv[-1]
     result = []
-    buf = 1
     for i in range(node, node+x):
         index = node + i
         index = index % len(selectList)
-        result.append(selectList[index-buf])
-        buf += 2
+        result.append(selectList[index])
     return result
 
 
-def sendPacket(nodes, x):
+def random_sample(selectList, x, *argv):
+    return random.sample(selectList, x)
+
+
+def send_packet(nodes, x, sample):
     queue_nodes = [0]
     count = 0
     for node in queue_nodes:
@@ -22,7 +25,7 @@ def sendPacket(nodes, x):
         selectList = nodes*1
         if node in selectList:
             selectList.remove(node)
-        for new_node in random.sample(selectList, x):
+        for new_node in sample(selectList, x, node):
             if new_node not in queue_nodes:
                 queue_nodes.append(new_node)
         if len(queue_nodes) == len(nodes):
@@ -30,7 +33,7 @@ def sendPacket(nodes, x):
     return 0, count
 
 
-def sendPacketQueue(nodes, x):
+def send_packet_queue(nodes, x, sample):
     queue_packets = [[0]]
     scope_visited_nodes = [0]
     count = 0
@@ -40,7 +43,7 @@ def sendPacketQueue(nodes, x):
         for visited_node in packet:
             if visited_node in selectList:
                 selectList.remove(visited_node)
-        for new_node in random.sample(selectList, x):
+        for new_node in sample(selectList, x):
             if new_node not in scope_visited_nodes:
                 new_packet = [new_node]
                 new_packet.extend(packet)
@@ -51,35 +54,19 @@ def sendPacketQueue(nodes, x):
     return 0, count
 
 
-def sendPacketChoice(nodes, x):
-    queue_nodes = [0]
-    count = 0
-    for node in queue_nodes:
-        count += 1
-        selectList = nodes*1
-        if node in selectList:
-            selectList.remove(node)
-        for new_node in myChoice(selectList, x, node):
-            if new_node not in queue_nodes:
-                queue_nodes.append(new_node)
-        if len(queue_nodes) == len(nodes):
-            return 1, count
-    return 0, count
-
-
 def run_epidemic(x, n, i, *argv):
     start = time.time()
     count_success = 0
     count_iteration = 0
     nodes = list(i for i in range(n))
+    algo = send_packet
+    sample = random_sample
     if 'with_queue' in argv:
-        algo = sendPacketQueue
-    elif 'with_choice' in argv:
-        algo = sendPacketChoice
-    else:
-        algo = sendPacket
+        algo = send_packet_queue
+    if 'with_choice' in argv:
+        sample = my_сhoice_sample
     for packet in range(i):
-        result = algo(nodes, x)
+        result = algo(nodes, x, sample)
         count_success += result[0]
         count_iteration += result[1]
     recived = format(count_success/i*100, ".2f")
